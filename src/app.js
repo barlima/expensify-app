54cf836409e931c0db3192ece4790ba57ab01263
Bartek <bartekperucki@gmail.com>
2018-08-22 15:56:44 +0200

    code update

--- src/app.js
+++ src/app.js
@@ -1,13 +1,5 @@
-// import './utils.js'
-import subtract, { square, add } from './utils.js';
-import isSenior, { isAdult, canDrink } from './person.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp.js';
 
-console.log('app.js is running');
-console.log(square(4));
-console.log(add(100, 23));
-console.log(subtract(100, 23));
-
-// console.log(isAdult(19));
-// console.log(canDrink(19));
-
-console.log(isSenior(60));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
---
+++ src/playground/app-import-export.js
@@ -0,0 +1,13 @@
+// import './utils.js'
+import subtract, { square, add } from '../utils.js';
+import isSenior, { isAdult, canDrink } from '../person.js';
+
+console.log('app.js is running');
+console.log(square(4));
+console.log(add(100, 23));
+console.log(subtract(100, 23));
+
+// console.log(isAdult(19));
+// console.log(canDrink(19));
+
+console.log(isSenior(60));
