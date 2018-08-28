fe43e4fa226821aa86b1d6cc2694828069d58e76
Bartek <bartekperucki@gmail.com>
2018-08-28 15:22:09 +0200

    Part 2 - Udemy React

--- src/app.js
+++ src/app.js
@@ -1,32 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp.js';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
-
-// class OldSyntax {
-//   constructor(props) { 
-//     this.name = "Mike";
-//   }
-//   getGreeting() {
-//     return `Hi. My name is ${this.name}`;
-//   }
-// }
-
-// const oldSyntax = new OldSyntax();
-// console.log(oldSyntax.getGreeting());
-
-// // ----------
-// // for the new syntax the binding is not necessary
-
-// class NewSyntax {
-//   name = 'Jen';
-//   getGreeting = () => {
-//     return `Hi. My name is ${this.name}`;
-//   }
-// }
-
-// const newSyntax = new NewSyntax();
-// console.log(newSyntax.getGreeting());
+ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
