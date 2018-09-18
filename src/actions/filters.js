a88f5f7d131bdd87fa299869f799481cd8db2d7d
barlima <bartekperucki@gmail.com>
2018-09-18 22:37:43 +0200

    React and Redux

---
+++ src/actions/filters.js
@@ -0,0 +1,34 @@
+// SET_TEXT_FILTER
+
+export const setTextFilter = ( text = '' ) => ({
+  type: 'SET_TEXT_FILTER',
+  text
+});
+
+// SORT_BY_DATE
+
+export const sortByDate = () => ({
+  type: 'SORT_BY',
+  sortBy: 'date'
+});
+
+// SORT_BY_AMOUNT
+
+export const sortByAmount = () => ({
+  type: 'SORT_BY',
+  sortBy: 'amount'
+});
+
+// SET_START_DATE
+
+export const setStartDate = ( date ) => ({
+  type: 'SET_START_DATE',
+  date
+});
+
+// SET_END_DATE
+
+export const setEndDate = ( date ) => ({
+  type: 'SET_END_DATE',
+  date
+});
--- src/app.js
+++ src/app.js
@@ -1,9 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter.js';
+import configureStore from './store/configureStore';
+import {addExpense} from './actions/expenses';
+import {setTextFilter} from './actions/filters';
+import getVisibleExpenses from './selectors/expenses'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+const store = configureStore();
 
+store.dispatch(addExpense({
+  description: 'Water bill',
+  amount: 150,
+  createdAt: 1000
+}));
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+store.dispatch(addExpense({
+  description: 'Gas bill',
+  amount: 3000,
+  createdAt: 1000
+}));
+
+store.dispatch(setTextFilter('Water'));
+
+setTimeout(() => {
+  store.dispatch(setTextFilter('rent'));
+}, 3000);
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
