2c9b11612a147781faec9cf235a372d578886499
barlima <bartekperucki@gmail.com>
2018-10-07 22:20:50 +0200

    Setup test database environment

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,33 +1,176 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyDTnI9jem0XXyM7XQg8iURfmFGnnFOZaeY",
-  authDomain: "expensify-dfa3f.firebaseapp.com",
-  databaseURL: "https://expensify-dfa3f.firebaseio.com",
-  projectId: "expensify-dfa3f",
-  storageBucket: "expensify-dfa3f.appspot.com",
-  messagingSenderId: "821774798115"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref().set({
-  name: "Bartek Perucki",
-  age: 25,
-  isSingle: false,
-  location: {
-    city: 'Krakow',
-    country: 'Poland'
-  }  
-}).then(() => {
-  console.log('Data is saved');
-}).catch((e) => {
-  console.log('This failed.', e);
-});
+export { firebase, database as default };
 
-database.ref('isSingle').set(null); // same as remove
+// // child_removed
+// database.ref('expenses').on('child_removed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+// // child_changed
+// database.ref('expenses').on('child_changed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+// // child_added
+// database.ref('expenses').on('child_added', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+// database.ref('expenses')
+//   .on('value', (snapshot) => {
+//     const expenses = [];
+
+//     snapshot.forEach((childSnapshot) => {
+//       expenses.push({
+//         id: childSnapshot.key,
+//         ...childSnapshot.val()
+//       });
+//     });
+
+//     console.log(expenses);
+// });
+
+// database.ref('expenses').push({
+//   description: 'Rent',
+//   note: '',
+//   amount: 109500,
+//   createdAt: 1000
+// });
+
+// database.ref('expenses').push({
+//   description: 'Coffee',
+//   note: '',
+//   amount: 350,
+//   createdAt: 0
+// });
+
+// database.ref('expenses').push({
+//   description: 'Fuel', 
+//   note: '',
+//   amount: 5000,
+//   createdAt: 0
+// });
+
+// database.ref('notes/-LO8mnDJME0qaXvpofxr').update({
+//   body: 'Buy food'
+// });
+
+// database.ref('notes').push({
+//   title: 'Course topis',
+//   body: 'React native, Python'
+// });
+
+// const firebaseNotes = {
+//   notes: {
+//     asdasa: {
+//       body: 'This is my note',
+//       title: 'First note'
+//     },
+//     adsfsf: {
+//       body: 'Another note',
+//       title: 'This is a note'
+//     }
+//   }
+// };
+
+// const notes = [{
+//   id: '12',
+//   body: 'This is my note',
+//   title: 'First note'
+// }, {
+//   id: '761sai',
+//   body: 'Another note',
+//   title: 'This is a note'
+// }];
+
+// database.ref('notes').set(notes);
+
+// const subscription = database.ref().on('value', (snapshot) => {
+//   const name = snapshot.val().name;
+//   const jobTitle = snapshot.val().job.title;
+//   const jobLactaion = snapshot.val().job.company;
+//   console.log(`${name} is a ${jobTitle} at ${jobLactaion}`);
+// });
+
+// setTimeout(() => {
+//   database.ref('name').set('Mike');
+// }, 3500);
+
+// const onValueCHange = database.ref().on('value', (snapshot) => {
+//   console.log(snapshot.val());
+// }, (e) => {
+//   console.log('Error with data fetching', e);
+// });
+
+// setTimeout(() => {
+//   database.ref('age').set(29);
+// }, 3500);
+
+// setTimeout(() => {
+//   database.ref().off(onValueCHange);
+// }, 7000);
+
+// setTimeout(() => {
+//   database.ref('age').set(30);
+// }, 10500);
+
+// database.ref('location/city')
+// database.ref()
+//   .once('value')
+//   .then((snapshot) => {
+//     const val = snapshot.val();
+//     console.log(val);
+//   })
+//   .catch((e) => {
+//     console.log('Error fetching data', e);
+//   });
+
+
+
+// database.ref().set({
+//   name: "Bartek Perucki",
+//   age: 25,
+//   stressLevel: 6,
+//   job: {
+//    title: 'Software developer',
+//    company: 'Google' 
+//   },
+//   location: {
+//     city: 'Krakow',
+//     country: 'Poland'
+//   }  
+// }).then(() => {
+//   console.log('Data is saved');
+// }).catch((e) => {
+//   console.log('This failed.', e);
+// });
+
+// // database.ref('isSingle').set(null); // same as remove
+// // database.ref().update({
+// //   job: 'Manager',
+// //   'location/city': 'Boston'
+// // });
+
+// database.ref().update({
+//   stressLevel: 9,
+//   'job/company': 'Amazon',
+//   'location/city': 'Seattle'
+// });
+
 
 // database.ref()
 //   .remove()
