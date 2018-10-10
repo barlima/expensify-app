b6fb1def27a399b53d558a3a52883f4fad1c649e
barlima <bartekperucki@gmail.com>
2018-10-10 23:10:29 +0200

    Up to s16e07

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -19,30 +19,38 @@ export class ExpenseListFilters extends React.Component {
 
   render () {
     return (
-      <div>
-        <input type="text" value={this.props.filters.text} onChange={(e) => {
-          this.props.setTextFilter(e.target.value);
-        }} />
-        <select value={this.props.filters.sortBy} onChange={(e) => {
-          if (e.target.value === "date") {
-            this.props.sortByDate();
-          } else if (e.target.value === "amount") {
-            this.props.sortByAmount();
-          }
-        }}>
-          <option value="date">Date</option>
-          <option value="amount">Amount</option>
-        </select>
-        <DateRangePicker 
-          startDate={this.props.filters.startDate}
-          endDate={this.props.filters.endDate}
-          onDatesChange={this.onDatesChange}
-          focusedInput={this.state.calendarFocused}
-          onFocusChange={this.onFocusChange}
-          showClearDates={true}
-          numberOfMonths={1}
-          isOutsideRange={() => false}
-        />
+      <div className="content-container">
+        <div className="input-group">
+          <div className="input-group__item">
+            <input className="text-input" type="text" value={this.props.filters.text} placeholder="Search Expenses" onChange={(e) => {
+              this.props.setTextFilter(e.target.value);
+            }} />
+          </div>
+          <div className="input-group__item">
+            <select className="select" value={this.props.filters.sortBy} onChange={(e) => {
+              if (e.target.value === "date") {
+                this.props.sortByDate();
+              } else if (e.target.value === "amount") {
+                this.props.sortByAmount();
+              }
+            }}>
+              <option value="date">Date</option>
+              <option value="amount">Amount</option>
+            </select>
+          </div>
+          <div className="input-group__item">
+            <DateRangePicker 
+              startDate={this.props.filters.startDate}
+              endDate={this.props.filters.endDate}
+              onDatesChange={this.onDatesChange}
+              focusedInput={this.state.calendarFocused}
+              onFocusChange={this.onFocusChange}
+              showClearDates={true}
+              numberOfMonths={1}
+              isOutsideRange={() => false}
+            />
+          </div>
+        </div>
       </div>
     )
   }
--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
 import pluralize from 'pluralize';
@@ -10,13 +11,15 @@ export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
   const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
 
   return (
-    <div>
-      { 
-        expenseCount !== 0 && 
-        <h1>
-          Viewing {expenseCount} {pluralize("expense", expenseCount)} totalling {formattedExpensesTotal}.
+    <div className="page-header">
+      <div className="content-container">
+        <h1 className="page-header__title">
+          Viewing <span>{expenseCount}</span> {pluralize("expense", expenseCount)} totalling <span>{formattedExpensesTotal}</span>
         </h1>
-      }
+        <div className="page-header__action">
+          <Link className="button" to="/create">Add Expense</Link>
+        </div>
+      </div>
     </div>
   );
 };
